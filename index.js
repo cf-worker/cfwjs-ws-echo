58d1d6a712e8e6c94e1b10dad9f9b687b126ab91
Marcos Neves <neves64@icloud.com>
2024-10-02 02:50:34 -0300

    add favicon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let startedAt = 0
 let counter = 0
+let clients = 0
 
 export default {
   /**
@@ -7,13 +8,18 @@ export default {
    */
   async fetch(request) {
     startedAt ||= Date.now()
-    const uptime = () => Math.round((Date.now() - startedAt) / 1000)
+    const uptime = () => elapsedHuman(Date.now() - startedAt)
+
+    if (request.url.endsWith("/favicon.ico")) {
+      return emptyFaviconResponse()
+    }
 
     const upgradeHeader = request.headers.get("Upgrade")
     if (upgradeHeader !== "websocket") {
       return Response.json({
         startedAt: new Date(startedAt).toISOString(),
         counter,
+        clients,
         uptime: uptime()
       })
       // return new Response('Expected Upgrade: websocket', { status: 426 })
@@ -27,6 +33,7 @@ export default {
       server.send(JSON.stringify({
         city: request.cf.city,
         colo: request.cf.colo,
+        clients,
         ip: request.headers.get("cf-connecting-ip") ?? request.headers.get("x-real-ip")
       }))
     }
@@ -38,7 +45,7 @@ export default {
         case "count": return server.send(++counter)
         case "now": return server.send(new Date().toISOString())
         case "boot": return server.send(new Date(startedAt).toISOString())
-        case "uptime": return server.send(uptime)
+        case "uptime": return server.send(uptime())
         case "help": return server.send(
           "ping, info, count, now, boot, uptime, /ping, /pong <data> /close 1000 [reason]"
         )
@@ -51,10 +58,37 @@ export default {
     // 1005 no status code was actually present
     // 1006 closed abnormally, without sending or receiving a Close control frame
     // 1009 Message is too large. Worker have a size limit of 1 MiB (1048576)
-    server.addEventListener("close", ({ type, code, reason, wasClean }) =>
+    server.addEventListener("close", ({ type, code, reason, wasClean }) => {
+      clients--
       console.log({ type, code, reason, wasClean })
-    )
+    })
     sendInfo()
+    clients++
     return new Response(null, { status: 101, webSocket: client })
   }
+}
+
+function elapsedHuman(ms) {
+  const s = Math.floor(ms / 1000)
+  const m = Math.floor(s / 60)
+  const h = Math.floor(m / 60)
+  const d = Math.floor(h / 24)
+  let str = ""
+  if (d > 0) str += `${d}d`
+  if (h > 0) str += `${h % 24}h`
+  if (m > 0) str += `${m % 60}m`
+  if (s > 0) str += `${s % 60}s`
+  else str += `${ms}ms`
+  return str
+}
+
+function emptyFaviconResponse() {
+  return new Response(null, {
+    status: 204,
+    statusText: "No Content",
+    headers: {
+      "Content-Type": "image/x-icon",
+      "Cache-Control": "public, max-age=15552000"
+    }
+  })
 }
